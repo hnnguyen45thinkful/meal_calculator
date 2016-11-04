c9217f77499f4ec5caeabdbb8cd3504590b8a3ca
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-04 01:55:20 -0700

    still working on the js meal calculator

--- meal_calculator.js
+++ meal_calculator.js
@@ -30,38 +30,16 @@ var dish = function(name, price) {
 	this.price = price;
 };
 
-var Steak = new dish('Steak', 15);
-var Fish = new dish('Fish', 10);
-var Pasta = new dish('Pasta', 9);
-var Ceviche = new dish('Ceviche', 13);
+var Pho = new dish('Pho', 15);
+var EggRolls = new dish('EggRolls', 10);
+var Noodles = new dish('Noodles', 9);
+var Dumplings = new dish('Dumplings', 13);
 var Cheese = new dish('Cheese', 8);
 var Calamari = new dish('Calamari', 10);
 
 
-var Hieu = new diner('Jon', [Steak, Calamari]);
-var Hanh = new diner('Jhon', [Fish, Ceviche]);
-var Hong = new diner('Jonny', [Cheese, Pasta]);
+var Hieu = new diner('Hieu', [Steak, Calamari]);
+var Hanh = new diner('Hanh', [Fish, Ceviche]);
+var Hong = new diner('Hong', [Cheese, Pasta]);
 
 
-var meal = function(tax,tip,diners){
- this.diners = diners;
- this.tip = tip;
- this.tax = tax;
- this.total = 0;
- this.subtotal = 0;
- var self = this;
- this.calculatePrice = function(){
-   this.diners.map(function(diner){
-     diner.dishes.map(function(dish){
-       self.subtotal += dish.price;
-     });
-   });
-   this.total = this.subtotal + (this.subtotal * this.tip) + (this.subtotal * this.tax);
- };
-};
-meal = new meal(0.6,0.20,[Jon,Jhon,Jonny]);
-meal.calculatePrice();
-console.log('Subtotal:',meal.subtotal, '($) dollars');
-console.log('Tax:',meal.tax, 'percent');
-console.log('Tip:',meal.tip, 'percent');
-console.log('Total:',meal.total, '($) dollars');
